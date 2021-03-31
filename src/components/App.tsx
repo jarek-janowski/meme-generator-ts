1c16ba3acaf19e6b697c9ca2c8edb450092e6670
jarek-janowski <jarjanowski95@gmail.com>
2021-03-31 16:20:41 +0200

    feat: add fetch from imgflip API & display meme img

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,33 @@
+import {useState, useEffect} from 'react'
 import './App.css';
+
+interface MemeProps {
+  
+}
+ 
+const Meme: React.FC<MemeProps> = () => {
+
+  const [memes, setMemes] = useState([] as any[])
+
+  useEffect(() => {
+    fetch('https://api.imgflip.com/get_memes')
+    .then(res => res.json())
+    .then(data => {
+      const memes = data.data.memes
+      setMemes(memes)
+    })
+  }, []);
+
+  return ( 
+    memes.length ? <img src={memes[0].url } alt={memes[0].name}/> : <></>
+   );
+}
+ 
+
 
 const App = () => {
   return (
-    <div className="App">
-      hi
-    </div>
+   <Meme />
   );
 }
 
