70b2e0da7a3b4058381b67054adf99a8f03de6c4
jarek-janowski <jarjanowski95@gmail.com>
2021-03-31 18:03:01 +0200

    feat: add function to update captions from inputs

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,63 @@
 import {useState, useEffect} from 'react'
 import './App.css';
 
-interface MemeProps {
+const Meme = () => {
   
-}
- 
-const Meme: React.FC<MemeProps> = () => {
 
-  
   const [memes, setMemes] = useState([] as any[])
   const [memeIndex, setMemeIndex] = useState(0);
-  
-
-  // console.log(Math.floor(Math.random() * 100))
-
+  const [captions, setCaptions] = useState([] as any)
+ 
   useEffect(() => {
     fetch('https://api.imgflip.com/get_memes')
     .then(res => res.json())
     .then(data => {
       const memes = data.data.memes
       setMemes(memes)
-      const randomMeme = Math.floor(Math.random() * memes.length)
-      setMemeIndex(randomMeme)
+      console.log(memes)
+      const randomMemeIndex = Math.floor(Math.random() * memes.length)
+      setMemeIndex(randomMemeIndex)
     })
   }, []);
+
+  useEffect(() => {
+    if(memes.length) {
+      setCaptions(Array(memes[memeIndex].box_count).fill(''));
+    }
+  }, [memeIndex, memes]);
+
+  useEffect(() => {
+    console.log(captions)
+  }, [captions]);
+
+  const updateCaption = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+    const text = e.target.value || '';
+    setCaptions(
+      captions.map((caption: [], i: number) =>{
+        if(index === i){
+          return text
+        }else {
+          return caption
+        }
+      })
+    )
+  }
 
   return ( 
     memes.length
     ?<div className="memes">
+      <button onClick={() => console.log('Generate')}>Generate</button>
       <button onClick={() => setMemeIndex(memeIndex + 1)}>Skip</button>
-      <img style={{maxWidth: 500, maxHeight: 500}} src={memes[memeIndex].url } alt={memes[memeIndex].name}/> 
+      {
+        captions.map((caption: [], index: number) =>(
+          <input onChange={(e) => updateCaption(e, index)} key={index}/>
+        ))
+      }
+      <img 
+        style={{maxWidth: 500, maxHeight: 500}} 
+        src={memes[memeIndex].url } 
+        alt={memes[memeIndex].name}
+      /> 
     </div>
     : <></>
    );
